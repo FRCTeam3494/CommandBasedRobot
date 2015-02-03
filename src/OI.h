93f3d487d75c381549ba3c67985b4e85ba30fde1
kmateti <kiron.mateti@gmail.com>
2015-02-03 13:23:05 -0500

    Simplified OI - initial Joystick() test

--- src/OI.h
+++ src/OI.h
@@ -1,27 +1,17 @@
 #ifndef OI_H
 #define OI_H
 
-
 #include "WPILib.h"
-
 
 class OI
 {
 private:
-
+	Joystick* lstick;
+	Joystick* rstick;
 public:
 	OI();
-	//declare the contoller
-	Joystick* controller;
-	JoystickButton* buttonA;
-	JoystickButton* buttonB;
-	JoystickButton* buttonX;
-	JoystickButton* buttonY;
-	JoystickButton* buttonLB;
-	JoystickButton* buttonRB;
-	JoystickButton* buttonStart;
-	JoystickButton* buttonSelect;
-
+	float GetLeftJoystick();
+	float GetRightJoystick();
 };
 
 #endif
