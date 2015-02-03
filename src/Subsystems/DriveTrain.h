d26f165ebecab99e23ed48186559bd7f64e6d69e
kmateti <kiron.mateti@gmail.com>
2015-02-03 13:37:33 -0500

    CANTalon can be private

--- src/Subsystems/DriveTrain.h
+++ src/Subsystems/DriveTrain.h
@@ -4,6 +4,30 @@
 #include "Commands/Subsystem.h"
 #include "WPILib.h"
 
+class DriveTrain: public Subsystem
+{
+private:
+	// It's desirable that everything possible under private except
+	// for methods that implement subsystem capabilities
+	CANTalon* talonLeftMaster;
+	CANTalon* talonLeftFollower;
+	CANTalon* talonRightMaster;
+	CANTalon* talonRightFollower;
+
+public:
+	DriveTrain();
+	void InitDefaultCommand();
+	void TankDrive(float leftAxis, float rightAxis);
+};
+
+#endif
+
+/*#ifndef DriveTrain_H
+#define DriveTrain_H
+
+#include "Commands/Subsystem.h"
+#include "WPILib.h"
+
 class DriveTrain: public Subsystem {
 private:
 	// It's desirable that everything possible under private except
@@ -22,3 +46,4 @@ public:
 };
 
 #endif
+*/
