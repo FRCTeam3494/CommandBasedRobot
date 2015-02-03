c51605c99932a7f3d33984a5397e0aa8a68088be
kmateti <kiron.mateti@gmail.com>
2015-02-03 13:19:58 -0500

    Added DriveTrain Subsystem

--- src/CommandBase.cpp
+++ src/CommandBase.cpp
@@ -1,10 +1,9 @@
 #include "CommandBase.h"
-#include "Subsystems/ExampleSubsystem.h"
-#include "Commands/Scheduler.h"
 #include "Subsystems/DriveTrain.h"
+#include "Commands/Scheduler.h"
 
 // Initialize a single static instance of all of your subsystems to NULL
-
+DriveTrain* CommandBase::driveTrain = NULL;
 OI* CommandBase::oi = NULL;
 
 CommandBase::CommandBase(char const *name) :
@@ -23,6 +22,6 @@ void CommandBase::init()
 	// Create a single static instance of all of your subsystems. The following
 	// line should be repeated for each subsystem in the project.
 
-
+	driveTrain = new DriveTrain();
 	oi = new OI();
 }
